53136aa304dfe1cc68d6eede99b41703119acb2f
juanmanuelcuellarbahamon1234 <juanmanuelcuellarbahamon1234@gmail.com>
2019-08-20 15:36:41 -0500

    configuracion de estilos

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/usuario', (req,res) =>{
-    res.render('index');
+    res.render('inicio');
 });
 
 router.post('/add', (req,res) =>{
