15500d376cec5acfeb2a83295771f7f79a9af42d
juanmanuelcuellarbahamon1234 <juanmanuelcuellarbahamon1234@gmail.com>
2019-08-21 19:08:20 -0500

    diseño index

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,19 +2,23 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req,res) =>{
-    res.render('hola');
+    res.render('vistas/index');
 });
 
-router.post('/add', (req,res) =>{
+router.get('/registroUsuario', (req,res) =>{
+    res.render('vistas/registroUsuario');
+});
 
-    var nombre = req.body.nombre;
-    var apellido = req.body.apellido;
-    
-    res.render('index',{
-        nombre: nombre,
-        apellido: apellido
-    });
+router.get('/ingresoUsuario', (req,res) =>{
+    res.render('vistas/ingresoUsuario');
+});
 
+router.get('/index-usuario', (req,res) =>{
+    res.render('vistas/index-usuario');
+});
+
+router.get('/UsuarioUI', (req,res) =>{
+    res.render('vistas/usuarios/usuarioUI')
 });
 
 module.exports = router;
