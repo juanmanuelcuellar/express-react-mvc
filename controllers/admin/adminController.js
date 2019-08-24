aec4558907df8e01cddf55f4c1d9440564035f54
juanmanuelcuellarbahamon <47219351+juanmanuelcuellarbahamon@users.noreply.github.com>
2019-08-24 14:46:33 -0500

    adminUI, EmpleadoUI, UsuarioUI

--- controllers/admin/adminController.js
+++ controllers/admin/adminController.js
@@ -13,6 +13,18 @@ router.get('/AdminUI/empleados', (req,res) =>{
     res.render('vistas/admin/empleados')
 });
 
+router.get('/AdminUI/empleados/agregar', (req,res) =>{
+    res.render('vistas/admin/agregarEmpleado')
+});
+
+router.get('/AdminUI/productos/agregar', (req,res) =>{
+    res.render('vistas/admin/agregarProducto')
+});
+
+router.get('/AdminUI/productos/actualizar', (req,res) =>{
+    res.render('vistas/admin/actualizarProductos')
+})
+
 router.get('/AdminUI/productos', (req,res) =>{
     res.render('vistas/admin/productos')
 });
